2c4b3fb733ec5c2b3b7733c3d2104c2bfa294810
Saurabh Kumar <khankingc799@gmail.com>
2025-08-31 18:25:45 +0530

    update app.py

--- app.py
+++ app.py
@@ -311,10 +311,14 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
 
+
+
 if __name__ == "__main__":
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
 
     init_db()  # har run pe db check/create hoga
 
-    socketio.run(app, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
